5e465eab1ef71a7abb04210cdcc25ee21f506cff
Brad Roodt <brad.roodt@gmail.com>
2020-07-06 10:06:25 +0200

    feat: adds composition api example

---
+++ src/components/Counter.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <button @click="increment">
+             {{state.someText}} is: {{ state.count }}, double is: {{ state.double }}
+    
+        </button>
+  <p>{{someOtherNumber}}</p>
+    </div>
+  
+</template>
+
+<script lang="ts">
+import { defineComponent, Ref } from "@vue/composition-api";
+import { reactive, ref, computed } from "@vue/composition-api";
+
+interface StateInterface {
+  count: number;
+  double: number;
+  someText: string;
+}
+
+export default defineComponent({
+  setup() {
+    const state: StateInterface = reactive({
+      count: 0,
+      double: computed(() => state.count * 2),
+      someText: "Bob"
+    });
+
+    const someOtherNumber: Ref<number> = ref(10);
+
+    function increment(): void {
+      state.count++;
+    }
+
+    return {
+      state,
+      someOtherNumber,
+      increment
+    };
+  }
+});
+</script>
