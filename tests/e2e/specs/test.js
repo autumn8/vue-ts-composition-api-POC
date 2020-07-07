5c68a3d4d3896781cb36dfb95869c639488f9d39
Brad Roodt <brad.roodt@gmail.com>
2020-07-07 14:20:20 +0200

    test: updates posts test

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -3,7 +3,17 @@
 describe("My First Test", () => {
   it("Visits the app root url", () => {
     cy.visit("/");
-    cy.get(".post").should('have.length', 2);
-    
+    cy.get(".post").should("have.length", 2);
+    cy.get(".v-text-field").type("is a developer{enter}");
+    cy.get(".post").should("have.length", 3);
+    cy.get(".post")
+      .eq(2)
+      .contains("is a developer");
+    cy.pause();
+    cy.get(".post")
+      .eq(2)
+      .click();
+    cy.pause();
+    cy.get(".post").should("have.length", 2);
   });
 });
